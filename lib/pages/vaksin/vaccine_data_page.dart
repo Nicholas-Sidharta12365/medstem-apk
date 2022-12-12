2fe0a6664a4ff3dd8cff59afb66f22dda0e49afb
Rafif1103 <rafifrahmadika@gmail.com>
2022-12-12 12:56:33 +0700

    add edit_dose

--- lib/pages/vaksin/vaccine_data_page.dart
+++ lib/pages/vaksin/vaccine_data_page.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:medstem/model/vaccine_data.dart';
 import 'package:medstem/pages/vaksin/vaccine_add.dart';
+import 'package:medstem/pages/vaksin/vaccine_user.dart';
+import 'package:medstem/pages/vaksin/edit_dose.dart';
+import 'package:medstem/login.dart';
 
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
@@ -11,6 +14,8 @@ import 'package:pbp_django_auth/pbp_django_auth.dart';
 class VaccineDetails {
   static late Fields _getDetails;
   static Fields get fetcher => _getDetails;
+  static late List<String> _dataSaved = [];
+  static List<String> get dataSaved => _dataSaved;
 }
 
 class VaccineDataPage extends StatefulWidget {
@@ -23,13 +28,14 @@ class VaccineDataPage extends StatefulWidget {
 class _VaccineDataPageState extends State<VaccineDataPage> {
   Future<List<VaccineData>> fetchVaccineData(CookieRequest request) async {
     // decode the response into the json form
-    var data = await request.get('https://medstem.up.railway.app/vaksin/json1/');
+    var data = await request.get('https://medstem.up.railway.app/vaksin/json/');
 
     // convert the json data into VaccineData object
     List<VaccineData> listVaccineData = [];
     for (var d in data) {
       if (d != null) {
         listVaccineData.add(VaccineData.fromJson(d));
+        VaccineDetails._dataSaved.add(VaccineData.fromJson(d).fields.name);
       }
     }
     return listVaccineData;
@@ -38,125 +44,23 @@ class _VaccineDataPageState extends State<VaccineDataPage> {
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
+    bool status = request.loggedIn;
+    ScrollController _controller = new ScrollController();
 
     return Scaffold(
         appBar: AppBar(
           title: const Text('Data Vaksin'),
         ),
         drawer: DrawerClass(),
-        body: Column(children: [
-        FutureBuilder(
-            future: fetchVaccineData(request),
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                // print("null geming");
-                return const Center(child: CircularProgressIndicator());
-                // return const Text("404 Sadge");
-              } else {
-                if (!snapshot.hasData) {
-                  return Column(
-                    children: const [
-                      Text(
-                        "Data Vaksin Kosong",
-                        style:
-                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                      ),
-                      SizedBox(height: 8),
-                    ],
-                  );
-                } else {
-                  return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (_, index) => Container(
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 12),
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(15.0),
-                                boxShadow: const [
-                                  BoxShadow(
-                                      color: Colors.black, blurRadius: 2.0)
-                                ]),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                TextButton(
-                                  onPressed: () {
-                                    VaccineDetails._getDetails = snapshot.data![index].fields;
-                                    showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return Dialog(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    elevation: 15,
-                                    child: Container(
-                                      child: ListView(
-                                        padding: const EdgeInsets.only(top: 20, bottom: 20),
-                                        shrinkWrap: true,
-                                        children: <Widget>[
-                                          Center(child: const Text('Vaccine Details')),
-                                          SizedBox(height: 20),
-                                          // TODO: Munculkan informasi yang didapat dari form
-                                          Text(
-                                            VaccineDetails.fetcher.name.toString(),
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(fontSize: 24),
-                                            ),
-                                          Text(
-                                            "Posted By User ID : " + VaccineDetails.fetcher.user.toString(),
-                                            textAlign: TextAlign.center,
-                                            ),
-                                          Text(
-                                            VaccineDetails.fetcher.date.toString(),
-                                            textAlign: TextAlign.center,
-                                            ),
-                                          Text(
-                                            "Efek Samping : " + VaccineDetails.fetcher.sideEffect,
-                                            textAlign: TextAlign.center,
-                                            ),
-                                          Text(
-                                            "Total Dosis : " + VaccineDetails.fetcher.dose.toString() + " mg",
-                                            textAlign: TextAlign.center,
-                                            ),
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.pop(context);
-                                            },
-                                            child: Text('Kembali'),
-                                          ), 
-                                        ],
-                                      ),
-                                    ),
-                                  );
-                                });
-                                  },
-                                  child: Text(
-                                  "${snapshot.data![index].fields.name}",
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue,
-                                  ),
-                                  )
-                                ),
-                                const SizedBox(height: 10),
-                                //Spacer(),
-                                
-                              ],
-                            ),
-                          ));
-                }
-              }
-            }),
-            TextButton(
+        body: 
+        
+        child:Column(
+        // controller: _controller,
+          children: [
+              if (status)...[
+              TextButton(
                 child: const Text(
-                  "Add Vaksin",
+                  "Vaksin User",
                   style: TextStyle(color: Colors.white),
                 ),
                 style: ButtonStyle(
@@ -166,11 +70,197 @@ class _VaccineDataPageState extends State<VaccineDataPage> {
                   Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const VaccineAddPage()));
+                      builder: (context) => const VaccineUserPage()));
                 }
               ),
-            ])
+            FutureBuilder(
+                future: fetchVaccineData(request),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    // print("null geming");
+                    return const Center(child: CircularProgressIndicator());
+                    // return const Text("404 Sadge");
+                  } else {
+                    if (!snapshot.hasData) {
+                      return Column(
+                        children: const [
+                          Text(
+                            "Data Vaksin Kosong",
+                            style:
+                                TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                          ),
+                          SizedBox(height: 8),
+                        ],
+                      );
+                    } else {
+                      return ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (_, index) => Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 12),
+                                padding: const EdgeInsets.all(20.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                          color: Colors.black, blurRadius: 2.0)
+                                    ]),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        VaccineDetails._getDetails = snapshot.data![index].fields;
+                                        showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return Dialog(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        elevation: 15,
+                                        child: Container(
+                                          child: ListView(
+                                            padding: const EdgeInsets.only(top: 20, bottom: 20),
+                                            shrinkWrap: true,
+                                            children: <Widget>[
+                                              Center(child: const Text('Vaccine Details')),
+                                              SizedBox(height: 20),
+                                              // TODO: Munculkan informasi yang didapat dari form
+                                              Text(
+                                                VaccineDetails.fetcher.name.toString(),
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(fontSize: 24),
+                                                ),
+                                              Text(
+                                                "Posted By User ID : " + VaccineDetails.fetcher.user.toString(),
+                                                textAlign: TextAlign.center,
+                                                ),
+                                              Text(
+                                                VaccineDetails.fetcher.date.toString(),
+                                                textAlign: TextAlign.center,
+                                                ),
+                                              Text(
+                                                "Efek Samping : " + VaccineDetails.fetcher.sideEffect,
+                                                textAlign: TextAlign.center,
+                                                ),
+                                              Text(
+                                                "Total Dosis : " + VaccineDetails.fetcher.dose.toString() + " mg",
+                                                textAlign: TextAlign.center,
+                                                ),
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: Text('Kembali'),
+                                              ), 
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    });
+                                      },
+                                      child: Text(
+                                      "${snapshot.data![index].fields.name}",
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blue,
+                                      ),
+                                      )
+                                    ),
+                                    const SizedBox(height: 10),
+                                    //Spacer(),
+                                    
+                                  ],
+                                ),
+                              ));
+                    }
+                  }
+                }),
+              
+                Center(
+                    child: Row (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 4, left:4),
+                    child: 
+                    TextButton(
+                        child: const Text(
+                          "Add Vaksin",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const VaccineAddPage()));
+                        }
+                      ),
+                    ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 4, left:4),
+                        child: 
+                      TextButton(
+                          child: const Text(
+                            "Edit Dose",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.blue),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const EditDosePage()));
+                          }
+                        ),
+                        )
+                    ],)
+                  )
+              ] else ...[
+                Center(child: Column(children: [
+                  const Text(
+                    "Please Log In First",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                TextButton(
+                    child: const Text(
+                      "Log In",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()));
+                    }
+                  ),
+                ]),
+                )
+              ]    
+            ],)
 
-        );
+        
+            );
+
   }
 }
---
+++ lib/pages/vaksin/vaccine_user.dart
@@ -0,0 +1,179 @@
+import 'dart:convert';
+import 'package:medstem/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:medstem/model/vaccine_data.dart';
+import 'package:medstem/pages/vaksin/vaccine_data_page.dart';
+
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+
+class VaccineDetailsUser {
+  static late Fields _getDetails;
+  static Fields get fetcher => _getDetails;
+}
+
+class VaccineUserPage extends StatefulWidget {
+  const VaccineUserPage({Key? key}) : super(key: key);
+
+  @override
+  _VaccineUserPageState createState() => _VaccineUserPageState();
+}
+
+class _VaccineUserPageState extends State<VaccineUserPage> {
+  Future<List<VaccineData>> fetchVaccineUserData(CookieRequest request) async {
+    // decode the response into the json form
+    var data = await request.get('https://medstem.up.railway.app/vaksin/json1/');
+
+    // convert the json data into VaccineData object
+    List<VaccineData> listVaccineData = [];
+    for (var d in data) {
+      if (d != null) {
+        listVaccineData.add(VaccineData.fromJson(d));
+      }
+    }
+    return listVaccineData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Data Vaksin'),
+        ),
+        body: Column(children: [
+          TextButton(
+                child: const Text(
+                  "Kembali",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
+                onPressed: () {
+                  Navigator.pop(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const VaccineDataPage()));
+                }
+              ),
+          Expanded(
+            flex: 2,
+            child:
+          FutureBuilder(
+              future: fetchVaccineUserData(request),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  // print("null geming");
+                  return const Center(child: CircularProgressIndicator());
+                  // return const Text("404 Sadge");
+                } else {
+                  if (!snapshot.hasData) {
+                    return Column(
+                      children: const [
+                        Text(
+                          "Data Vaksin Kosong",
+                          style:
+                              TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                        ),
+                        SizedBox(height: 8),
+                      ],
+                    );
+                  } else {
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (_, index) => Container(
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 12),
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Colors.black, blurRadius: 2.0)
+                                  ]),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      VaccineDetailsUser._getDetails = snapshot.data![index].fields;
+                                      showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return Dialog(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      elevation: 15,
+                                      child: Container(
+                                        child: ListView(
+                                          padding: const EdgeInsets.only(top: 20, bottom: 20),
+                                          shrinkWrap: true,
+                                          children: <Widget>[
+                                            Center(child: const Text('Vaccine Details')),
+                                            SizedBox(height: 20),
+                                            // TODO: Munculkan informasi yang didapat dari form
+                                            Text(
+                                              VaccineDetailsUser.fetcher.name.toString(),
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(fontSize: 24),
+                                              ),
+                                            Text(
+                                              VaccineDetailsUser.fetcher.date.toString(),
+                                              textAlign: TextAlign.center,
+                                              ),
+                                            Text(
+                                              "Efek Samping : " + VaccineDetailsUser.fetcher.sideEffect,
+                                              textAlign: TextAlign.center,
+                                              ),
+                                            Text(
+                                              "Total Dosis : " + VaccineDetailsUser.fetcher.dose.toString() + " mg",
+                                              textAlign: TextAlign.center,
+                                              ),
+                                            Text(
+                                              "Stok Vaksin : " + VaccineDetailsUser.fetcher.stock.toString(),
+                                              textAlign: TextAlign.center,
+                                              ),
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('Kembali'),
+                                            ), 
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  });
+                                    },
+                                    child: Text(
+                                    "${snapshot.data![index].fields.name}",
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue,
+                                    ),
+                                    )
+                                  ),
+                                  const SizedBox(height: 10),
+                                  //Spacer(),
+                                  
+                                ],
+                              ),
+                            ));
+                  }
+                }
+              }),
+          ),
+            ])
+
+        );
+  }
+}
